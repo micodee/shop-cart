4ea69c0e776d382ccad5c60ffc566ec1f7617ccb
micodee <martomhidayat>
2022-11-10 22:32:37 +0700

    error switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Headers from './containers/Header'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import ProductListing from './containers/ProductListing'
+import ProductDetail from './containers/ProductDetail'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+    <Headers/>
+    <Switch>
+        <Route path='/' exact compnonent={ProductListing}/>
+        <Route path='/product/:productId' exact compnonent={ProductDetail}/>
+        <Route>404 Not Found</Route>
+    </Switch>
+    </Router>
   )
 }
 
